5b4c987932c2c295aed6349537a18eaf7689ebae
Fernando Guillen <fguillen.mail@gmail.com>
2014-02-09 19:35:05 +0100

    Fixing tests

--- test/helpers/admin/admin_helper_test.rb
+++ test/helpers/admin/admin_helper_test.rb
@@ -18,16 +18,16 @@ class Admin::AdminHelperTest < ActionView::TestCase
     assert_equal("active", admin_menu_class(:admin_users))
 
     request_mock.stubs(:fullpath).returns("/admin")
-    assert_equal("active", admin_menu_class(:items))
+    assert_equal("active", admin_menu_class(:short_film_users))
 
-    request_mock.stubs(:fullpath).returns("/admin/items")
-    assert_equal("active", admin_menu_class(:items))
+    request_mock.stubs(:fullpath).returns("/admin/short_film_users")
+    assert_equal("active", admin_menu_class(:short_film_users))
 
-    request_mock.stubs(:fullpath).returns("/admin/items/1")
-    assert_equal("active", admin_menu_class(:items))
+    request_mock.stubs(:fullpath).returns("/admin/short_film_users/1")
+    assert_equal("active", admin_menu_class(:short_film_users))
 
-    request_mock.stubs(:fullpath).returns("/admin/items/1/edit")
-    assert_equal("active", admin_menu_class(:items))
+    request_mock.stubs(:fullpath).returns("/admin/short_film_users/1/edit")
+    assert_equal("active", admin_menu_class(:short_film_users))
 
     request_mock.stubs(:fullpath).returns("/admin/log_book_events")
     assert_equal("active", admin_menu_class(:log_book_events))
