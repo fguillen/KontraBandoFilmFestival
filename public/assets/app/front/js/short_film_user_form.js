076f6f74119f00e65ec347fedc967e0836ba3833
Fernando Guillen <fguillen.mail@gmail.com>
2014-02-16 23:10:20 +0100

    Uncommented form validator js

--- public/assets/app/front/js/short_film_user_form.js
+++ public/assets/app/front/js/short_film_user_form.js
@@ -1,15 +1,15 @@
-// console.log("short_film_user_form.js");
+console.log("short_film_user_form.js");
 
-// $(function(){
-//   if( $("#short_film_user_form").length > 0 ) {
-//     $("#short_film_user_form").validationEngine("init");
+$(function(){
+  if( $("#short_film_user_form").length > 0 ) {
+    $("#short_film_user_form").validationEngine("init");
 
-//     $("#short_film_user_form").submit(function (event) {
-//       var form_builder_url = $("#contact_form_url").val();
+    $("#short_film_user_form").submit(function (event) {
+      var form_builder_url = $("#contact_form_url").val();
 
-//       if(!$("#short_film_user_form").validationEngine("validate")) {
-//         event.preventDefault();
-//       }
-//     });
-//   }
-// })
+      if(!$("#short_film_user_form").validationEngine("validate")) {
+        event.preventDefault();
+      }
+    });
+  }
+})
